c5d20c8d4e266c99c57c63bf96b46188b0f2a826
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-08-14 22:45:54 -0400

    pulling data from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('123')
+
+
+
+/////function to pull data from openweathermap API
+
+async function recievelocationdata(location){
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ff02d978e63fdb995bf6241ddbf8661b`, {mode: 'cors'});
+    const weatherData = await response.json()
+    console.log(weatherData.name);
+}
+
+recievelocationdata('Toronto');
--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,22 @@ module.exports = {
 };
 module.exports = {
     mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }}
+      ],
+    },
   };
+
