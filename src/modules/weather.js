7f4b204da1bd0c64f004be0453c19299f1519f25
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-08-16 00:23:40 -0400

    restructured code and restarted

---
+++ src/modules/weather.js
@@ -0,0 +1,23 @@
+/////function to pull data from openweathermap API
+const weather = (() => {
+async function recieveLocationData(location){
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ff02d978e63fdb995bf6241ddbf8661b`, {mode: 'cors'});
+    const weatherData = await response.json()
+    if (weatherData.cod != 200)
+        console.log('Response not found');
+    const data = pullData(weatherData)
+    return data;
+}
+/////pulls relevant data from api call
+function pullData(obj) {
+    const {name: name,
+           main: {temp: temp, feels_like: feelslike, humidity: humidity},
+           sys: {country: country},
+           wind: {speed: windspeed}
+                        } = obj;
+    return {name, temp, feelslike, humidity, country, windspeed}
+}
+})();
+
+
+export default weather
