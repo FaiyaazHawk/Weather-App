8752caadef39af9763e8d1083278499a0f13078a
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-08-26 22:20:31 -0400

    fixed to public output for vercel

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ searchform.addEventListener("submit", (e) => {
 
 inputbtn.addEventListener("click", async () => {
     if (inputform.value === "") return;
+    console.log(inputform.value)
     const weatherData = await weather.recieveLocationData(inputform.value);
     display.setDisplay(weatherData);
   });
